6013a25bfcb5f68b48fd27fb050cbcc7ed1c90f7
Kevin Huang <zhenlong.x.huang@gmail.com>
2016-11-27 19:24:25 +0800

    Solved "301. Remove Invalid Parentheses".

---
+++ algorithms/src/main/java/com/leetcode/RemoveInvalidParentheses.java
@@ -0,0 +1,70 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/remove-invalid-parentheses/
+ */
+public class RemoveInvalidParentheses {
+    public List<String> removeInvalidParentheses(String s) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        List<String> result = new LinkedList<>();
+
+        queue.add(s);
+        visited.add(s);
+
+        boolean found = false;
+
+        while(!queue.isEmpty()) {
+            String str = queue.poll();
+
+            if(isVaild(str)) {
+                found = true;
+                result.add(str);
+            }
+
+            if (found) { // Continue to find the same length valid string.
+                continue;
+            }
+
+            for(int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) != '(' && str.charAt(i) != ')') { // Ignore invalid char
+                    continue;
+                }
+
+                String tmp = str.substring(0, i) + str.substring(i+1);
+                if (!visited.contains(tmp)) {
+                    queue.add(tmp);
+                    visited.add(tmp);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isVaild(String str) {
+        int count = 0;
+
+        for(char c : str.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+                if (count < 0) {
+                    return false;
+                }
+            }
+        }
+
+        return count == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses("()())()"));
+        System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses("(a)())()"));
+        System.out.println(new RemoveInvalidParentheses().removeInvalidParentheses(")("));
+    }
+
+}
