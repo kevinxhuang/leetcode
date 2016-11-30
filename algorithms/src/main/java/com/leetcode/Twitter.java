5fe4679d8474092a9da3e6a166e55a0e45fa3528
Kevin Huang <zhenlong.x.huang@gmail.com>
2016-11-30 21:02:21 +0800

    Solved "355. Design Twitter".

---
+++ algorithms/src/main/java/com/leetcode/Twitter.java
@@ -0,0 +1,93 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/design-twitter/
+ */
+public class Twitter {
+    private final Set<Integer> users;
+    private final Map<Integer, Set<Integer>> links;
+    private final Map<Integer, Integer> orderTable;
+    private final Map<Integer, Set<Integer>> userToTweets;
+
+    /** Initialize your data structure here. */
+    public Twitter() {
+        users = new HashSet<>();
+        links = new HashMap<>();
+        orderTable = new HashMap<>();
+        userToTweets = new HashMap<>();
+    }
+
+    /** Compose a new tweet. */
+    public void postTweet(int userId, int tweetId) {
+        ensureUserExists(userId);
+        orderTable.put(tweetId, orderTable.size() + 1);
+        userToTweets.get(userId).add(tweetId);
+    }
+
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    public List<Integer> getNewsFeed(int userId) {
+        if (!users.contains(userId)) {
+            return Collections.EMPTY_LIST;
+        }
+
+        Set<Integer> recentTweets = new TreeSet<>((tweetId1, tweetId2) -> orderTable.get(tweetId2) - orderTable.get(tweetId1));
+
+        recentTweets.addAll(userToTweets.get(userId));
+        for(int followeeId : links.get(userId)) {
+            recentTweets.addAll(userToTweets.get(followeeId));
+        }
+
+        return new ArrayList(recentTweets).subList(0, Math.min(recentTweets.size(), 10));
+    }
+
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    public void follow(int followerId, int followeeId) {
+        ensureUserExists(followerId);
+        ensureUserExists(followeeId);
+        links.get(followerId).add(followeeId);
+    }
+
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    public void unfollow(int followerId, int followeeId) {
+        ensureUserExists(followerId);
+        ensureUserExists(followeeId);
+        links.get(followerId).remove(followeeId);
+    }
+
+    private void ensureUserExists(int userId) {
+        if (!users.contains(userId)) {
+            users.add(userId);
+            links.put(userId, new HashSet<>());
+            userToTweets.put(userId, new HashSet<>());
+        }
+    }
+
+    public static void main (String[] args) {
+        Twitter twitter = new Twitter();
+
+        // User 1 posts a new tweet (id = 5).
+        twitter.postTweet(1, 5);
+
+        // User 1's news feed should return a list with 1 tweet id -> [5].
+        twitter.getNewsFeed(1);
+
+        // User 1 follows user 2.
+        twitter.follow(1, 2);
+
+        // User 2 posts a new tweet (id = 6).
+        twitter.postTweet(2, 6);
+
+        // User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+        // Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+        System.out.println(twitter.getNewsFeed(1));
+
+        // User 1 unfollows user 2.
+        twitter.unfollow(1, 2);
+
+        // User 1's news feed should return a list with 1 tweet id -> [5],
+        // since user 1 is no longer following user 2.
+        System.out.println(twitter.getNewsFeed(1));
+    }
+}
