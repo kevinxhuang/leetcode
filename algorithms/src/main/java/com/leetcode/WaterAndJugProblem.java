604fefa3f2eaca3b3b81a217bb3504eef8a257d2
Kevin Huang <zhenlong.x.huang@gmail.com>
2016-12-10 11:05:50 +0800

    Solved "365. Water and Jug Problem".

---
+++ algorithms/src/main/java/com/leetcode/WaterAndJugProblem.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+/**
+ * https://leetcode.com/problems/water-and-jug-problem/
+ */
+public class WaterAndJugProblem {
+	public boolean canMeasureWater(int x, int y, int z) {
+		if (x + y < z) {
+			return false;
+		}
+
+		if (x == z || y == z || x + y == z) {
+			return true;
+		}
+
+		return z % gcd(x, y) == 0;
+	}
+
+	public int gcd(int a, int b){
+		while(b != 0 ){
+			int tmp = b;
+			b = a%b;
+			a = tmp;
+		}
+		return a;
+	}
+}
