08e7cbf0fb0dfd6251b9b78aa3688250158346e7
Kevin Huang <zhenlong.x.huang@gmail.com>
2016-12-26 22:49:51 +0800

    Solved "110. Balanced Binary Tree".

---
+++ algorithms/src/main/java/com/leetcode/FindTheDuplicateNumber.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ *
+ * Idea from https://discuss.leetcode.com/topic/25913/my-easy-understood-solution-with-o-n-time-and-o-1-space-without-modifying-the-array-with-clear-explanation
+ */
+public class FindTheDuplicateNumber {
+	public int findDuplicate(int[] nums) {
+		int slow = nums[0];
+		int fast = nums[nums[0]];
+		while (slow != fast)
+		{
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		}
+
+		fast = 0;
+		while (fast != slow)
+		{
+			fast = nums[fast];
+			slow = nums[slow];
+		}
+		return slow;
+	}
+}
