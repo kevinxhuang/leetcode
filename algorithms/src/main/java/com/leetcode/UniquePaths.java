2db0e526d5129b9e7ff4e80d88c2734940dc055c
Kevin Huang <zhenlong.x.huang@gmail.com>
2017-02-28 21:42:12 +0800

    Solved "62. Unique Paths".

---
+++ algorithms/src/main/java/com/leetcode/UniquePaths.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+/**
+ * https://leetcode.com/problems/unique-paths/?tab=Description
+ */
+public class UniquePaths {
+	public int uniquePaths(int m, int n) {
+		int[][] dp = new int[m][n];
+
+		dp[0][0] = 1;
+		for(int i = 0; i < m; i++) {
+			dp[i][0] = 1;
+		}
+		for(int i = 0; i < n; i++) {
+			dp[0][i] = 1;
+		}
+
+		for(int i = 1; i < m; i++) {
+			for(int j = 1; j < n; j++) {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1];
+			}
+		}
+
+		return dp[m-1][n-1];
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new UniquePaths().uniquePaths(1, 1));
+		System.out.println(new UniquePaths().uniquePaths(2, 2));
+		System.out.println(new UniquePaths().uniquePaths(2, 3));
+		System.out.println(new UniquePaths().uniquePaths(3, 2));
+		System.out.println(new UniquePaths().uniquePaths(3, 3));
+	}
+}
