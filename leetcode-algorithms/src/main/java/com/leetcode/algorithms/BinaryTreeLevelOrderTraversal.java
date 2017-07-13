a42f52a3e0398939f70a64421c6577dc9fe06516
Kevin Huang <zhenlong.x.huang@gmail.com>
2017-07-13 21:47:11 +0800

    Solved "102. Binary Tree Level Order Traversal".

---
+++ leetcode-algorithms/src/main/java/com/leetcode/algorithms/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,50 @@
+package com.leetcode.algorithms;
+
+import com.leetcode.algorithms.model.TreeNode;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+/**
+ * https://leetcode.com/problems/binary-tree-level-order-traversal
+ */
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return Collections.EMPTY_LIST;
+        }
+
+        List<List<Integer>> result = new LinkedList<>();
+
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            List<TreeNode> nodesOfCurrentLevel = new LinkedList<>();
+            List<TreeNode> nodesOfNextLevel = new LinkedList<>();
+
+            while(!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                nodesOfCurrentLevel.add(node);
+                if (node.left != null) {
+                    nodesOfNextLevel.add(node.left);
+                }
+                if (node.right != null) {
+                    nodesOfNextLevel.add(node.right);
+                }
+            }
+
+            if (nodesOfCurrentLevel.size() > 0) {
+                result.add(nodesOfCurrentLevel.stream().map(node -> node.val).collect(Collectors.toList()));
+            }
+
+            if (nodesOfNextLevel.size() > 0) {
+                queue.addAll(nodesOfNextLevel);
+            }
+        }
+
+        return result;
+    }
+}
