93cdd99741eb857ef365d956f2912cf50919a8b2
Kevin Huang <zhenlong.x.huang@gmail.com>
2018-02-27 20:25:53 +0800

    Solved `669. Trim a Binary Search Tree`.

---
+++ leetcode-algorithms/src/main/scala/com/leetcode/algorithms/TrimABinarySearchTree.scala
@@ -0,0 +1,26 @@
+package com.leetcode.algorithms
+
+import com.leetcode.algorithms.scalamodel.TreeNode
+
+/**
+  * 669. Trim a Binary Search Tree
+  *
+  * https://leetcode.com/problems/trim-a-binary-search-tree
+  */
+object TrimABinarySearchTree {
+  def trimBST(root: TreeNode, L: Int, R: Int): TreeNode = {
+    if (root == null) {
+      null
+    } else {
+      if(root.value < L) {
+        trimBST(root.right, L, R)
+      } else if(root.value > R) {
+        trimBST(root.left, L, R)
+      } else {
+        root.left = trimBST(root.left, L, R)
+        root.right = trimBST(root.right, L, R)
+        root
+      }
+    }
+  }
+}
