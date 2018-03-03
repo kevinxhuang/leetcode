23dc1c3d637258295723747bee4dad946a33ba12
Kevin Huang <zhenlong.x.huang@gmail.com>
2018-03-03 09:36:45 +0800

    Solved `189. Rotate Array`.

---
+++ leetcode-algorithms/src/main/scala/com/leetcode/algorithms/RotateArray.scala
@@ -0,0 +1,17 @@
+package com.leetcode.algorithms
+
+/**
+  * 189. Rotate Array
+  *
+  * https://leetcode.com/problems/rotate-array
+  */
+object RotateArray {
+  def rotate(nums: Array[Int], k: Int): Unit = {
+    if(nums.isEmpty || k == 0) {
+      return
+    }
+
+    val newArray = nums.splitAt(nums.length - k % nums.length)._2 ++ nums.splitAt(nums.length - k % nums.length)._1
+    newArray.copyToArray(nums)
+  }
+}
