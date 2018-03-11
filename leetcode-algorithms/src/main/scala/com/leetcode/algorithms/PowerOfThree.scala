faa6c3db9809587875a701ac4dc42aebaae25b56
Kevin Huang <zhenlong.x.huang@gmail.com>
2018-03-11 14:26:15 +0800

    Solved `326. Power of Three`.

---
+++ leetcode-algorithms/src/main/scala/com/leetcode/algorithms/PowerOfThree.scala
@@ -0,0 +1,25 @@
+package com.leetcode.algorithms
+
+/**
+  * 326. Power of Three
+  *
+  * https://leetcode.com/problems/power-of-three
+  */
+object PowerOfThree {
+  def isPowerOfThree(n: Int): Boolean = {
+    n match {
+      case 0 => false
+      case 1 | 3 => true
+      case x =>
+        if(x % 3 != 0) {
+          false
+        } else {
+          if(math.cbrt(x).isValidInt) {
+            isPowerOfThree(math.cbrt(x).toInt)
+          } else {
+            isPowerOfThree(x / 3)
+          }
+        }
+    }
+  }
+}
