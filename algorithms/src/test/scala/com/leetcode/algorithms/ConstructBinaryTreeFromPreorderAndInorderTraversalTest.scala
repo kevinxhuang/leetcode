43574a6d7ced8bb47b937bd9cede40ebfb35cf7e
Zhenlong Huang <zhenlohuang@gmail.com>
2019-08-24 16:18:41 +0800

    Fix compilation error.

--- algorithms/src/test/scala/com/leetcode/algorithms/ConstructBinaryTreeFromPreorderAndInorderTraversalTest.scala
+++ algorithms/src/test/scala/com/leetcode/algorithms/ConstructBinaryTreeFromPreorderAndInorderTraversalTest.scala
@@ -3,6 +3,6 @@ package com.leetcode.algorithms
 
 class ConstructBinaryTreeFromPreorderAndInorderTraversalTest extends AlgorithmsTest {
   "ConstructBinaryTreeFromPreorderAndInorderTraversal" should "pass basic test" in {
-    ConstructBinaryTreeFromPreorderAndInorderTraversal.buildTree(Array(3,9,20,15,7), Array(9,3,15,20,7))
+    new ConstructBinaryTreeFromPreorderAndInorderTraversal().buildTree(Array(3,9,20,15,7), Array(9,3,15,20,7))
   }
 }
